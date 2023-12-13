096a3577e0ccd220fedd04832753ec054733d88a
JonniBig <bigun.i88@gmail.com>
2023-12-13 14:30:21 +0200

    test

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,28 +1,23 @@
-import React, { Suspense } from 'react';
+import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Layout from './Layout/Layout';
 import { HOME_ROUTE, MOUNTAIN_ROUTE } from 'constants/routes';
 import ScrollToTopButton from './Scroll/Scroll';
-
-const Home = React.lazy(() => import('pages/Home/Home'));
-const MountDetails = React.lazy(() =>
-  import('pages/MountDetails/MountDetails')
-);
-const NotFound = React.lazy(() => import('pages/NotFound'));
+import Home from 'pages/Home/Home';
+import NotFound from 'pages/NotFound';
+import MountDetails from 'pages/MountDetails/MountDetails';
 
 export const App = () => {
   return (
     <Layout>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
-          <Route path={HOME_ROUTE} element={<Home />} />
-          <Route
-            path={`${MOUNTAIN_ROUTE}/:mountainName`}
-            element={<MountDetails />}
-          />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </Suspense>
+      <Routes>
+        <Route path={HOME_ROUTE} element={<Home />} />
+        <Route
+          path={`${MOUNTAIN_ROUTE}/:mountainName`}
+          element={<MountDetails />}
+        />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
       <ScrollToTopButton />
     </Layout>
   );
