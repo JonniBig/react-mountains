d8d9901fc4d16137bc94cf936ef6dca0c01d9ed8
JonniBig <bigun.i88@gmail.com>
2023-12-13 14:07:44 +0200

    update: test;

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom';
 import Layout from './Layout/Layout';
 import { HOME_ROUTE, MOUNTAIN_ROUTE } from 'constants/routes';
 import ScrollToTopButton from './Scroll/Scroll';
+
 const Home = React.lazy(() => import('pages/Home/Home'));
 const MountDetails = React.lazy(() => import('pages/MountDetails'));
 const NotFound = React.lazy(() => import('pages/NotFound'));
--- src/pages/MountDetails.jsx
+++ src/pages/MountDetails.jsx
@@ -9,41 +9,47 @@ const MountDetails = () => {
   const mountainData = mountains.find(
     mountain => mountainName === mountain.mountainName
   );
+  console.log('mountainData: ', mountainData);
   const galleryImages = mountainData.gallery?.map(image => ({
     original: image,
     thumbnail: image,
   }));
+  console.log('galleryImages: ', galleryImages);
+
   useEffect(() => {
     window.scrollTo({ top: 0, behavior: 'smooth' });
   }, []);
+
   return (
-    <StyledMountDetails $backgroundimg={mountainData.thumbs[0]}>
-      {mountainData !== undefined && (
-        <div className="gallery">
-          <div className="innerContent">
-            <div className="discription">
-              <h1 className="title">{mountainName}</h1>
-              <p>
-                Lorem ipsum dolor sanimi, quo magnam expedita perferendis
-                assumenda.
-              </p>
+    <div>
+      <StyledMountDetails $backgroundimg={mountainData.thumbs[0]}>
+        {mountainData !== undefined && (
+          <div className="gallery">
+            <div className="innerContent">
+              <div className="discription">
+                <h1 className="title">{mountainName}</h1>
+                <p>
+                  Lorem ipsum dolor sanimi, quo magnam expedita perferendis
+                  assumenda.
+                </p>
+              </div>
+            </div>
+            <div>
+              <ReactImageGallery
+                autoPlay={false}
+                showPlayButton={false}
+                slideOnThumbnailOver={true}
+                lazyLoad={true}
+                disableKeyDown={false}
+                slideDuration={1000}
+                thumbnailPosition="top"
+                items={galleryImages}
+              />
             </div>
           </div>
-          <div>
-            <ReactImageGallery
-              autoPlay={false}
-              showPlayButton={false}
-              slideOnThumbnailOver={true}
-              lazyLoad={true}
-              disableKeyDown={false}
-              slideDuration={1000}
-              thumbnailPosition="top"
-              items={galleryImages}
-            />
-          </div>
-        </div>
-      )}
-    </StyledMountDetails>
+        )}
+      </StyledMountDetails>
+    </div>
   );
 };
 
